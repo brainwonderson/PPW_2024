0c4bfe7add5582fe366057935c13217feaed8487
beatriceasiahaan <keepfightingbebet@gmail.com>
2024-11-26 20:57:44 +0700

    Update dhpm.blade.php

--- resources/views/div-BEM/dhpm.blade.php
+++ resources/views/div-BEM/dhpm.blade.php
@@ -86,34 +86,52 @@
             <div class="relative bg-gray-200 p-6 rounded-lg text-center transform transition duration-300 hover:bg-gray-300 hover:scale-105 program-card cursor-pointer">
                 Membantu Mencari Sponsor dalam Kegiatan Besar yang Akan Dilakukan oleh BEM IT Del
                 <div class="tooltip absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-teal-500 text-white text-sm p-3 rounded-lg shadow-lg">
-                    Mahasiswa diberikan wadah untuk menanamkan atau mengingatkan kembali megenai keberagaman budaya Indonesia terkhusus 8 etnik budaya yang ada pada Sumut dan juga menamkan kembali jiwa nasionalisme. Dilaksanakan pada GSG dan dimulai dari pukul 13.00 s/d 18.00(selesai).
+                    Sponsor yang akan dilakukan  akan membantu dana BEM IT Del dalam penyelenggaraan acara dan memperkenalkan IT Del menjadi semakin luas di instansi Perusahaan atau pun Yayasan.
                     <div class="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-teal-500"></div>
                 </div>
             </div>
 
             <!-- Program Card 5 -->
             <div class="relative bg-gray-200 p-6 rounded-lg text-center transform transition duration-300 hover:bg-gray-300 hover:scale-105 program-card cursor-pointer">
-                Del Art Competition
+                Sponsor yang Akan Dilakukan Akan Membantu Dana BEM IT Del dalam Penyelenggaraan Acara dan Memperkenalkan IT Del Menjadi Semakin Luas di Instansi Perusahaan ataupun Yayasan
                 <div class="tooltip absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-teal-500 text-white text-sm p-3 rounded-lg shadow-lg">
-                    Mahasiswa diberi wadah untuk menyalurkan bakat dan menumbuhkan jiwa kompetisi serta meningkatkan kreativitas dan inovasi dari Mahasiswa dan merupakan salah satu acara untuk menambahkan softskill Mahasiswa yang berguna untuk menghadapi Industri 4.0, dengan mengangkat tema : Corak Nusantara. Penampilan atau karya peserta akan ditampilkan secara langsung dan akan di upload melalui akun Youtube dan Instagram DEPSENBUD.
+                    Program ini dilaksanakan untuk melakukan pengabdian kepada lingkungan sekitar kampus, seperti mengutip sampah yang berada di sekitar kampus IT Del.
                     <div class="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-teal-500"></div>
                 </div>
             </div>
             
             <!-- Program Card 6 -->
             <div class="relative bg-gray-200 p-6 rounded-lg text-center transform transition duration-300 hover:bg-gray-300 hover:scale-105 program-card cursor-pointer">
-                Konser
+                Mengadakan Penyuluhan Terkait Kesadaran akan Sampah ke Sekolah-Sekolah di Balige dan Sekitarnya
                 <div class="tooltip absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-teal-500 text-white text-sm p-3 rounded-lg shadow-lg">
-                Konser adalah suatu pertunjukan langsung, biasanya musik, di depan penonton. Musik dapat dimainkan oleh musikus tunggal, kadang disebut resital, atau suatu ensembel musik, seperti orkestra, paduan suara, atau grup musik. Di manapun dilangsungkan, musisi biasanya tampil di atas suatu panggung.
+                    Program ini dilaksanakan untuk menyampaikan kepada para siswa akan kesadaran terhadap sampah di lingkungan sekitar mereka.
                     <div class="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-teal-500"></div>
                 </div>
             </div>
 
             <!-- Program Card 7 -->
             <div class="relative bg-gray-200 p-6 rounded-lg text-center transform transition duration-300 hover:bg-gray-300 hover:scale-105 program-card cursor-pointer">
-                Makrab
+                Mengadakan Kerjasama dengan Media Massa
                 <div class="tooltip absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-teal-500 text-white text-sm p-3 rounded-lg shadow-lg">
-                    Makrab merupakan program kerja internal dari Depsenbud yang bertujuan untuk mengakrabkan dan lebih mengenal antar anggota dalam Depsenbud. Kegiatan ini diisi dengan kegiatan-kegiatan yang mendukung bagaimana caranya agar anggota-anggota bisa lebih dekat dan lebih akrab khususnya anggota baru. Kegiatan yang dilakukan yakni permainan berkelompok, sharing pendapat, makan bersama sampai camping bersama sama dalam satu tempat. Dimana kegiatan ini akan dilakukan di luar lingkungan kampus dan dilaksanakan ketika weekend atau libur kuliah.
+                Program ini dilakukan untuk memperluas pengaruh del bukan hanya di daerah Tobasa, diharapkan dapat mencapai luar provinsi. 
+                    <div class="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-teal-500"></div>
+                </div>
+            </div>
+
+            <!-- Program Card 8 -->
+            <div class="relative bg-gray-200 p-6 rounded-lg text-center transform transition duration-300 hover:bg-gray-300 hover:scale-105 program-card cursor-pointer">
+                Mengadakan Kerjasama dengan Pemerintah Tobasa
+                <div class="tooltip absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-teal-500 text-white text-sm p-3 rounded-lg shadow-lg">
+                    Program ini dilakukan untuk menciptakan dan membangun kepedulian mahasiswa/i IT Del terhadap event yang diadakan di wilayah Tobasa melalui partisipasi dalam program yang disusun oleh pemerintah Tobasa.
+                    <div class="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-teal-500"></div>
+                </div>
+            </div>
+
+            <!-- Program Card 9 -->
+            <div class="relative bg-gray-200 p-6 rounded-lg text-center transform transition duration-300 hover:bg-gray-300 hover:scale-105 program-card cursor-pointer">
+                Mengadakan Donasi Buku
+                <div class="tooltip absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-teal-500 text-white text-sm p-3 rounded-lg shadow-lg">
+                    Program kerja ini bertujuan untuk menggalang donasi buku dan mendistribusikannya ke rumah baca. Kegiatan ini melibatkan partisipasi mahasiswa dalam memberikan buku-buku bekas yang masih layak baca agar dapat dimanfaatkan oleh lebih banyak orang, terutama anak-anak dan remaja yang memiliki akses terbatas terhadap bahan bacaan.
                     <div class="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-teal-500"></div>
                 </div>
             </div>
